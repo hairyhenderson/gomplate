484603e1663105851e5e195f2cb3ea915bd7b8c2
Dave Henderson <dhenderson@gmail.com>
2016-04-12 13:22:52 -0400

    New functions join, title, toLower, and toUpper
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- typeconv.go
+++ typeconv.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"strconv"
+	"strings"
 )
 
 // TypeConv - type conversion function
@@ -43,4 +45,42 @@ func (t *TypeConv) JSONArray(in string) []interface{} {
 // Slice creates a slice from a bunch of arguments
 func (t *TypeConv) Slice(args ...interface{}) []interface{} {
 	return args
+}
+
+// Join concatenates the elements of a to create a single string.
+// The separator string sep is placed between elements in the resulting string.
+//
+// This is functionally identical to strings.Join, except that each element is
+// coerced to a string first
+func (t *TypeConv) Join(a []interface{}, sep string) string {
+	b := make([]string, len(a))
+	for i := range a {
+		b[i] = toString(a[i])
+	}
+	return strings.Join(b, sep)
+}
+
+func toString(in interface{}) string {
+	if s, ok := in.(string); ok {
+		return s
+	}
+	if s, ok := in.(fmt.Stringer); ok {
+		return s.String()
+	}
+	if i, ok := in.(int); ok {
+		return strconv.Itoa(i)
+	}
+	if u, ok := in.(uint64); ok {
+		return strconv.FormatUint(u, 10)
+	}
+	if f, ok := in.(float64); ok {
+		return strconv.FormatFloat(f, 'f', -1, 64)
+	}
+	if b, ok := in.(bool); ok {
+		return strconv.FormatBool(b)
+	}
+	if in == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("%s", in)
 }
