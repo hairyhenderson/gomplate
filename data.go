e4dc5a3ba37fe894939fc42ce300baae70f93f27
Dave Henderson <dhenderson@gmail.com>
2016-07-13 23:11:52 -0400

    Adding support for HTTP/HTTPS datasources
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- data.go
+++ data.go
@@ -5,11 +5,13 @@ import (
 	"io/ioutil"
 	"log"
 	"mime"
+	"net/http"
 	"net/url"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/blang/vfs"
 )
@@ -19,6 +21,20 @@ func init() {
 	mime.AddExtensionType(".json", "application/json")
 	mime.AddExtensionType(".yml", "application/yaml")
 	mime.AddExtensionType(".yaml", "application/yaml")
+
+	sourceReaders = make(map[string]func(*Source) ([]byte, error))
+
+	// Register our source-reader functions
+	addSourceReader("http", readHTTP)
+	addSourceReader("https", readHTTP)
+	addSourceReader("file", readFile)
+}
+
+var sourceReaders map[string]func(*Source) ([]byte, error)
+
+// addSourceReader -
+func addSourceReader(scheme string, readFunc func(*Source) ([]byte, error)) {
+	sourceReaders[scheme] = readFunc
 }
 
 // Data -
@@ -49,7 +65,8 @@ type Source struct {
 	URL   *url.URL
 	Ext   string
 	Type  string
-	FS    vfs.Filesystem
+	FS    vfs.Filesystem // used for file: URLs, nil otherwise
+	HC    *http.Client   // used for http[s]: URLs, nil otherwise
 }
 
 // NewSource - builds a &Source
@@ -128,7 +145,7 @@ func (d *Data) Datasource(alias string) map[string]interface{} {
 	source := d.Sources[alias]
 	b, err := d.ReadSource(source.FS, source)
 	if err != nil {
-		log.Fatalf("Couldn't read datasource '%s': %#v", alias, err)
+		log.Fatalf("Couldn't read datasource '%s': %s", alias, err)
 	}
 	if source.Type == "application/json" {
 		ty := &TypeConv{}
@@ -151,34 +168,69 @@ func (d *Data) ReadSource(fs vfs.Filesystem, source *Source) ([]byte, error) {
 	if ok {
 		return cached, nil
 	}
-	if source.URL.Scheme == "file" {
-		if fs == nil {
-			fs = vfs.OS()
-			source.FS = fs
-		}
-
-		// make sure we can access the file
-		_, err := fs.Stat(source.URL.Path)
+	if r, ok := sourceReaders[source.URL.Scheme]; ok {
+		data, err := r(source)
 		if err != nil {
-			log.Fatalf("Can't stat %s: %#v", source.URL.Path, err)
 			return nil, err
 		}
-
-		f, err := fs.OpenFile(source.URL.Path, os.O_RDWR, 0)
-		if err != nil {
-			log.Fatalf("Can't open %s: %#v", source.URL.Path, err)
-			return nil, err
-		}
-
-		b, err := ioutil.ReadAll(f)
-		if err != nil {
-			log.Fatalf("Can't read %s: %#v", source.URL.Path, err)
-			return nil, err
-		}
-		d.cache[source.Alias] = b
-		return b, nil
+		d.cache[source.Alias] = data
+		return data, nil
 	}
 
 	log.Fatalf("Datasources with scheme %s not yet supported", source.URL.Scheme)
 	return nil, nil
+}
+
+func readFile(source *Source) ([]byte, error) {
+	if source.FS == nil {
+		source.FS = vfs.OS()
+	}
+
+	// make sure we can access the file
+	_, err := source.FS.Stat(source.URL.Path)
+	if err != nil {
+		log.Fatalf("Can't stat %s: %#v", source.URL.Path, err)
+		return nil, err
+	}
+
+	f, err := source.FS.OpenFile(source.URL.Path, os.O_RDWR, 0)
+	if err != nil {
+		log.Fatalf("Can't open %s: %#v", source.URL.Path, err)
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatalf("Can't read %s: %#v", source.URL.Path, err)
+		return nil, err
+	}
+	return b, nil
+}
+
+func readHTTP(source *Source) ([]byte, error) {
+	if source.HC == nil {
+		source.HC = &http.Client{Timeout: time.Second * 5}
+	}
+	res, err := source.HC.Get(source.URL.String())
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		err := fmt.Errorf("Unexpected HTTP status %d on GET from %s: %s", res.StatusCode, source.URL, string(body))
+		return nil, err
+	}
+	ctypeHdr := res.Header.Get("Content-Type")
+	if ctypeHdr != "" {
+		mediatype, _, e := mime.ParseMediaType(ctypeHdr)
+		if e != nil {
+			return nil, e
+		}
+		source.Type = mediatype
+	}
+	return body, nil
 }
