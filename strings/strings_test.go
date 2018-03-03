062b1b598adb43ca2a73c0fd5b89c485da90f347
Dave Henderson <dhenderson@gmail.com>
2018-03-02 21:13:00 -0500

    Adding unit test for zero-width indent
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- strings/strings_test.go
+++ strings/strings_test.go
@@ -9,6 +9,7 @@ import (
 func TestIndent(t *testing.T) {
 	actual := "hello\nworld\n!"
 	expected := "  hello\n  world\n  !"
+	assert.Equal(t, actual, Indent(0, "  ", actual))
 	assert.Equal(t, expected, Indent(1, "  ", actual))
 	assert.Equal(t, "\n", Indent(1, "  ", "\n"))
 	assert.Equal(t, "  foo\n", Indent(1, "  ", "foo\n"))
