6740473594e3cc0323d41648b6312b5bbc13cf87
Dave Henderson <dhenderson@gmail.com>
2019-11-25 20:43:56 -0500

    Use cobra's built-in --version flag instead of implementing separately
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- cmd/gomplate/main.go
+++ cmd/gomplate/main.go
@@ -7,7 +7,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -21,7 +20,6 @@ import (
 )
 
 var (
-	printVer bool
 	verbose  bool
 	execPipe bool
 	opts     gomplate.Config
@@ -29,10 +27,6 @@ var (
 
 	postRunInput *bytes.Buffer
 )
-
-func printVersion(name string) {
-	fmt.Printf("%s version %s\n", name, version.Version)
-}
 
 // postRunExec - if templating succeeds, the command following a '--' will be executed
 func postRunExec(cmd *cobra.Command, args []string) error {
@@ -101,12 +95,8 @@ func newGomplateCmd() *cobra.Command {
 		Use:     "gomplate",
 		Short:   "Process text files with Go templates",
 		PreRunE: validateOpts,
+		Version: version.Version,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if printVer {
-				printVersion(cmd.Name())
-				return nil
-			}
-
 			if v, _ := cmd.Flags().GetBool("verbose"); v {
 				zerolog.SetGlobalLevel(zerolog.DebugLevel)
 			}
@@ -169,8 +159,6 @@ func initFlags(command *cobra.Command) {
 	command.Flags().StringVar(&opts.RDelim, "right-delim", rdDefault, "override the default right-`delimiter` [$GOMPLATE_RIGHT_DELIM]")
 
 	command.Flags().BoolVarP(&verbose, "verbose", "V", false, "output extra information about what gomplate is doing")
-
-	command.Flags().BoolVarP(&printVer, "version", "v", false, "print the version")
 }
 
 func main() {
