a296ab9796f4452dd4327b6593254848319fb894
Dave Henderson <dhenderson@gmail.com>
2023-10-21 13:08:28 -0400

    Remove defers from main() to stop suppressing panics (#1851)
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- cmd/gomplate/main.go
+++ cmd/gomplate/main.go
@@ -11,16 +11,15 @@ import (
 )
 
 func main() {
-	exitCode := 0
-	// defer the exit first, so it executes last, to let the deferred cancel run
-	defer func() { os.Exit(exitCode) }()
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
+}
 
+func run() error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// need to strip os.Args[0] so we only pass the actual flags
-	err := cmd.Main(ctx, os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
-	if err != nil {
-		exitCode = 1
-	}
+	return cmd.Main(ctx, os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
 }
